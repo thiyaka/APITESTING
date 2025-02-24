e626e4d99553014df5d6bf8e4303753681ee3ff4
thiyakapriyan.m <thiyakapriyan.m@gmail.com>
2025-02-24 20:32:56 +0530

    initial commit

---
+++ src/main/java/apiTestingPractice/HTTPURLConnectionExample.java
@@ -0,0 +1,41 @@
+package apiTestingPractice;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+public class HTTPURLConnectionExample {
+	
+	public void getmethod() {
+		
+		try {
+			URL url= new URL("https://www.google.com/");
+			
+			HttpURLConnection connection= (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.connect();// This step is optional
+			
+			int Responsecode = connection.getResponseCode();
+			System.out.println("Response code is "+Responsecode);
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HTTPURLConnectionExample connectionExample= new HTTPURLConnectionExample();
+		connectionExample.getmethod();
+
+	}
+
+}
